0fc337ccec7f2d7a853408165f083b4f47953aee
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-17 19:54:34 -0300

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -19,6 +19,15 @@ declare global {
     };
 
     type ResourceCrudModeTypesAllowed = "list" | "create" | "edit";
+    type MachinesCrudModeTypesAllowed = "list" | "create" | "edit";
+
+    interface Machine {
+        id: string,
+        name: string,
+        power: number,
+        createdAt: string,
+        updatedAt: string,
+    }
 };
 
 export {};
