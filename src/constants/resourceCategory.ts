90381c14731184b43405f934a9d9cca18c7df905
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-17 19:54:18 -0300

    Create resourceCategory.ts

---
+++ src/constants/resourceCategory.ts
@@ -0,0 +1,19 @@
+export const RESOURCE_CATEGORY_OPTIONS = [
+    { value: "COMPONENT", label: "Componente" },
+    { value: "ELECTRICITY", label: "Eletricidade" },
+    { value: "GAS", label: "Gás" },
+    { value: "RAW_MATERIAL", label: "Matéria-prima" },
+    { value: "RETAIL", label: "Retalho" },
+    { value: "SILICATE", label: "Silicato" },
+    { value: "WATER", label: "Água" },
+];
+
+export const RESOURCE_CATEGORY_VALUES = {
+    "COMPONENT": "Componente",
+    "ELECTRICITY": "Eletricidade",
+    "GAS": "Gás",
+    "RAW_MATERIAL": "Matéria-prima",
+    "RETAIL": "Retalho",
+    "SILICATE": "Silicato",
+    "WATER": "Água",
+};
