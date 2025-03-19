a712fc64bef2850675ff21dbdd29ac55b446908f
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-18 23:15:33 -0300

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,15 @@
-import { useForm, SubmitHandler, Path } from "react-hook-form";
+import { useForm, SubmitHandler, Path, DefaultValues } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ZodType } from "zod";
 import Button from "./Button";
+import { useEffect } from "react";
 
 interface FormProps<T> {
     fields: FieldType[];
     schema: ZodType<T>;
     onSubmit: SubmitHandler<any>;
     submitButtonLabel?: string,
+    initialData?: Partial<T>,
 }
 
 const Form = <T extends Record<string, any>>({
@@ -15,14 +17,23 @@ const Form = <T extends Record<string, any>>({
     schema,
     onSubmit,
     submitButtonLabel,
+    initialData,
 }: FormProps<T>) => {
     const {
         register,
         handleSubmit,
+        reset,
         formState: { errors, isSubmitting },
     } = useForm<T>({
         resolver: zodResolver(schema),
+        defaultValues: initialData as DefaultValues<T> || {} as DefaultValues<T>,
     });
+
+    useEffect(() => {
+        if (initialData) {
+            reset(initialData as DefaultValues<T>);
+        }
+    }, [initialData, reset]);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="p-4 space-y-4 border rounded-md">
