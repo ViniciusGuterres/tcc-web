6ea01d94c506f530dc2021c7583b0d90f7360069
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-20 18:06:28 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React, { lazy } from "react";
 import { BrowserRouter as Router, createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "../components/Layout";
 import machinesRoutes from "./machinesRoutes";
+import resourcesRoutes from "./resourcesRoutes";
 
 // Lazy-loaded pages for better performance
 // const ResourcesCRUD = lazy(() => import("../pages/ResourcesCRUD"));
@@ -9,6 +10,7 @@ const Login = lazy(() => import("../pages/Login"));
 
 const router = createBrowserRouter([
     { path: "/login", element: <Login />},
+    resourcesRoutes,
     machinesRoutes,
     {path: "*", element: <Layout />},
 ]);
