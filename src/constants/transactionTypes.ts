067ae651c155ffaa91bdccce08819c2c71220747
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-20 22:16:00 -0300

    Create transactionTypes.ts

---
+++ src/constants/transactionTypes.ts
@@ -0,0 +1,9 @@
+export const TRANSACTION_TYPES_OPTIONS = [
+    { value: "INCOMING", label: "Entrada" },
+    { value: "OUTCOMING", label: "Saída" },
+];
+
+export const TRANSACTION_TYPES_VALUES = {
+    'INCOMING': 'Entrada',
+    'OUTCOMING': 'Saída',
+};
