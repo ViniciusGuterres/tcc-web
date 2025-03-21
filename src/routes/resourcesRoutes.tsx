bd7ee4ec09e538ada520c13a30f15142080ff4f0
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-20 22:23:22 -0300

    Update resourcesRoutes.tsx

--- src/routes/resourcesRoutes.tsx
+++ src/routes/resourcesRoutes.tsx
@@ -2,6 +2,7 @@ import { RouteObject } from "react-router";
 import { lazy, Suspense } from "react";
 import PrivateRoute from "./PrivateRoute";
 import Layout from "../components/Layout";
+import ResourceTransactionForm from "../pages/ResourcesCRUD/ResourceTransactionForm";
 
 const ListResources = lazy(() => import('../pages/ResourcesCRUD/ListResources'));
 const ResourceForm = lazy(() => import('../pages/ResourcesCRUD/ResourceForm'));
@@ -16,6 +17,9 @@ const resourcesRoutes: RouteObject = {
                 { path: "", element: <Suspense fallback={<div>Carregando...</div>}><ListResources /></Suspense> },
                 { path: "create", element: <Suspense fallback={<div>Carregando...</div>}><ResourceForm crudMode={'create'} /></Suspense> },
                 { path: "edit/:id", element: <Suspense fallback={<div>Carregando...</div>}><ResourceForm crudMode={'edit'} /></Suspense> },
+                // Transactions crud
+                { path: "createTransaction/:resourceID", element: <Suspense fallback={<div>Carregando...</div>}><ResourceTransactionForm crudMode={'create'} /></Suspense> },
+                { path: "editTransaction/:resourceID/:transactionID", element: <Suspense fallback={<div>Carregando...</div>}><ResourceTransactionForm crudMode={'create'} /></Suspense> },
             ]
         }
     ],
