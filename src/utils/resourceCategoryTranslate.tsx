91a1b4f8cdb7156fc32affdbc5df6d9a39a611c3
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-23 23:02:21 -0300

    Delete resourceCategoryTranslate.tsx

--- src/utils/resourceCategoryTranslate.tsx
+++
@@ -1,7 +0,0 @@
-import { RESOURCE_CATEGORY_VALUES } from "../constants/resourceCategory";
-
-function resourceCategoryTranslate(categoryKey) {
-    return RESOURCE_CATEGORY_VALUES[categoryKey];
-}
-
-export default resourceCategoryTranslate;
