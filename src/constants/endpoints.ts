51bdea22361f10ce9917712a2ac4f2cb1aa6a46a
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-24 18:18:45 -0300

    Create endpoints.ts

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,11 @@
+const resourcesEndPoint = 'resources';
+const transactionsEndPoint = 'transactions';
+const productLinesEndPoint = 'product-lines';
+
+const endPoints =  {
+    resourcesEndPoint,
+    transactionsEndPoint,
+    productLinesEndPoint,
+};
+
+export default endPoints;
