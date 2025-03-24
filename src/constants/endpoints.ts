247483ab2ba2229852f6516f04c1a323e98b6920
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-03-24 18:54:05 -0300

    Update endpoints.ts

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,11 +1,13 @@
 const resourcesEndPoint = 'resources';
 const transactionsEndPoint = 'transactions';
 const productLinesEndPoint = 'product-lines';
+const productTypesEndPoint = 'product-types';
 
 const endPoints =  {
     resourcesEndPoint,
     transactionsEndPoint,
     productLinesEndPoint,
+    productTypesEndPoint,
 };
 
 export default endPoints;
