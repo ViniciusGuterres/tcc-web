069859790f8316b2b9e24ede639e30758b3902da
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-04-01 21:01:37 -0300

    fix: Fixed bug to navigate to menu path

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -15,10 +15,10 @@ function SidebarMenu({ links }: Props) {
     const buildMenuLinks = () => {
         const linksListItem = links.map(({ label, link, icon }) => {
             return (
-                <li key={`menu_link_${label}_${Date.now()}`}>
+                <li key={`menu_link_${label}_${link}`}>
                     <Link
                         className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                        to={link}
+                        to={`/${link.replace(/^\/+/, "")}`}
                     >
                         <Icon 
                             iconClass={icon}
