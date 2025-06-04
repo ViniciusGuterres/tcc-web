a9d147704d6f39c39a674e675c0ef20aa6e74e1b
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-06-03 22:54:41 -0300

    Update glazesRoutes.tsx

--- src/routes/glazesRoutes.tsx
+++ src/routes/glazesRoutes.tsx
@@ -2,6 +2,7 @@ import { RouteObject } from "react-router";
 import { lazy, Suspense } from "react";
 import PrivateRoute from "./privateRoutes";
 import Layout from "../components/Layout";
+import GlazeForm from "../pages/GlazesCRUD/GlazeForm";
 
 const ListGlazes = lazy(() => import('../pages/GlazesCRUD/ListGlazes'));
 
@@ -13,6 +14,8 @@ const glazesRoutes: RouteObject = {
             element: <Layout />,
             children: [
                 { path: "", element: <Suspense fallback={<div>Carregando...</div>}><ListGlazes /></Suspense> },
+                { path: "create", element: <Suspense fallback={<div>Carregando...</div>}><GlazeForm crudMode={'create'} /></Suspense> },
+                { path: "edit/:id", element: <Suspense fallback={<div>Carregando...</div>}><GlazeForm crudMode={'edit'} /></Suspense> },
             ]
         }
     ],
